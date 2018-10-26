d78b4969074b1f73ee93d23f8caf500c72af2757
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2018-10-26 15:12:07 -0600

    Commit

--- src/DoubleLinkedList.java
+++ src/DoubleLinkedList.java
@@ -1,8 +1,8 @@
 /*
- * Now refactor your design so that it is a Double Linked List as the 
- * class above indicates. Make a branch in your repository and call it 
- * doubleLinkedList. Switch to this branch and develop the Double Linked List 
- * code there. Now you can switch functionality by switching branches. 
+ * Now refactor your design so that it is a Double Linked List as the class 
+ * above indicates. Make a branch in your repository and call it doubleLinkedList.
+ * Switch to this branch and develop the Double Linked List code there. 
+ * Now you can switch functionality by switching branches. 
  * 
  * Shafiq Zainuddin - 200342741
  */

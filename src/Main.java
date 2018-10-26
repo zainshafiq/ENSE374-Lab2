1e8a089fe34910f6a9728f3434f6568be6548c38
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2018-10-26 13:59:19 -0600

    Commit main

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/* Your assignment is to write a Linked List Program that has the 
+ * following requirements.
+ * 
+ * Runs a program which allows the user to:
+ * • Add a node public void addElement(ListElement le)
+ * • Retrieve a node public ListElement getElement(int index)
+ * • Delete a node public ListElement deleteElement(int index)
+ * • Print the linked list from the tail public void printLinkedListTail()
+ * • Print the linked list from the head public void printLinkedListHead()
+ * 
+ * Shafiq Zainuddin - 200342741
+ */

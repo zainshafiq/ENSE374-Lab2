41f594f37fa5f8e92384ce405af81f5cf6265c6a
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2018-10-26 11:40:42 -0600

    New Java

---
+++ src/LinkedList.java
@@ -0,0 +1,19 @@
+/*
+ * Now create an object of this class and access the members: 
+ * ListElement le = new ListElement();
+ * le.setData(5);
+ * 
+ * Your assignment is to write a Linked List Program that has the 
+ * following requirements.
+ * 
+ * Runs a program which allows the user to:
+ * • Add a node public void addElement(ListElement le)
+ * • Retrieve a node public ListElement getElement(int index)
+ * • Delete a node public ListElement deleteElement(int index)
+ * • Print the linked list from the tail public void printLinkedListTail()
+ * • Print the linked list from the head public void printLinkedListHead()
+ * 
+ * Shafiq Zainuddin - 200342741
+ */
+
+
